95dd8d83e617e02a86000c07b792f27753f2a4a9
ynrfin <3326885+ynrfin@users.noreply.github.com>
2022-06-10 05:56:37 +0700

    create handler to snippet/view & snippet/new. and return 404 for not /

--- main.go
+++ main.go
@@ -8,7 +8,24 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello World from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request)  {
+    // Check if current request URL path exactly matches "/". I fit doesn't
+    // then http.NotFound() function to send a 404 response to the client.
+    // Importantly,we then return from the ahndler. If we don't return the handler
+    // would keep executing and also write the "Hello from Snippetbox" message
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
     w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request)  {
+    w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request)  {
+    w.Write([]byte("create new snippet..."))
 }
 
 func main() {
@@ -16,6 +33,8 @@ func main() {
     // regiseter the home function as the handler for the "/" URL pattern
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
 
     // Use the http.ListenAndServe() function to start a new web server. We pass in
     // two parameters: the TCP network address to listen  on(in this case ":4000")
